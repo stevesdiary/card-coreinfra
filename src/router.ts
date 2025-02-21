618f56f95e72a06571c3ca1f2b50b43e67eb2df4
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-21 14:11:07 +0100

    Add card routes to main router

--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,12 @@ import express, { Router } from "express";
 import userRouter from "./modules/user/routes/user.route";
 import loginRouter from "./modules/user/routes/login.route";
 import registrationRouter from "./modules/user/routes/registration.route";
-
+import cardRouter from "./modules/card/routes/card.route";
 const router = Router();
 
 router.use("/register", registrationRouter);
 router.use("/user", userRouter);
 router.use("/log", loginRouter);
+router.use("/card", cardRouter);
 
 export default router;
