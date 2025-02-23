d283f5e76254fe43543eb9e04c9b182580b2bffb
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-23 03:44:11 +0100

    Refactor card profile model: Update card types and remove separate card model

--- src/modules/card/profile/card-profile.model.ts
+++ src/modules/card/profile/card-profile.model.ts
@@ -6,17 +6,15 @@ import {
   ForeignKey, 
   BelongsTo,
   BeforeCreate,
-  BeforeUpdate,
-  Validate
+  BeforeUpdate
 } from 'sequelize-typescript';
-import { Op } from 'sequelize';
 import { User } from '../../user/models/user.model';
 import { CardPinEncryptionService } from '../services/card-encryption.service';
 
 export enum CardType {
   VIRTUAL = 'VIRTUAL',
-  PHYSICAL = 'PHYSICAL',
-  PREPAID = 'PREPAID'
+  CREDIT = 'CREDIT',
+  DEBIT = 'DEBIT'
 }
 
 export enum Currency {
@@ -122,6 +120,13 @@ export class CardProfile extends Model {
   })
   balance?: number;
 
+    @Column({
+    type: DataType.ENUM(...Object.values(Currency)),
+    allowNull: false,
+    defaultValue: Currency.NGN
+  })
+  currency?: Currency;
+
   @BeforeCreate
   @BeforeUpdate
   static encryptPin(instance: CardProfile) {
@@ -140,101 +145,3 @@ export class CardProfile extends Model {
   @BelongsTo(() => User)
   user?: User;
 }
-
-@Table({
-  tableName: 'cards'
-})
-export class CardModel extends Model {
-  @Column({
-    type: DataType.UUID,
-    defaultValue: DataType.UUIDV4,
-    primaryKey: true
-  })
-  id?: string;
-
-  @ForeignKey(() => CardProfile)
-  @Column({
-    type: DataType.UUID,
-    allowNull: false
-  })
-  card_profile_id?: string;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: false
-  })
-  card_number?: string;
-
-  @Column({
-    type: DataType.STRING,
-    allowNull: false
-  })
-  card_holder_name?: string;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(CardType)),
-    allowNull: false,
-    defaultValue: CardType.VIRTUAL
-  })
-  card_type?: CardType;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(CardStatus)),
-    allowNull: false,
-    defaultValue: CardStatus.PENDING
-  })
-  status?: CardStatus;
-
-  @Column({
-    type: DataType.DATE,
-    allowNull: false
-  })
-  expiry_date?: Date;
-
-  @Column({
-    type: DataType.STRING
-  })
-  cvv?: string;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(Currency)),
-    allowNull: false,
-    defaultValue: Currency.NGN
-  })
-  currency?: Currency;
-
-  @Column({
-    type: DataType.DECIMAL(10, 2),
-    defaultValue: 0.00
-  })
-  balance?: number;
-
-  @Column({
-    type: DataType.ENUM(...Object.values(Branch_Blacklist)),
-    defaultValue: Branch_Blacklist.HEAD_OFFICE
-  })
-  branch_lacklist?: string;
-
-  @BelongsTo(() => CardProfile)
-  card_profile?: CardProfile;
-
-  @BeforeCreate
-  @BeforeUpdate
-  static async validateUniqueActiveCard(instance: CardProfile) {
-    const existingCard = await CardProfile.findOne({
-      where: {
-        user_id: instance.user_id,
-        card_type: instance.card_type,
-        status: CardStatus.ACTIVE,
-        expiry_date: {
-          [Op.gt]: new Date()
-        }
-      }
-    });
-
-    if (existingCard) {
-      throw new Error('You already have an active card of this type');
-    }
-  }
-}
-
