f7cf6c168538f9b818bb67553353729825ac62a3
Stephen Oyeyemi <94493468+stevesdiary@users.noreply.github.com>
2025-02-23 19:38:06 +0100

    Add fee controller with create, update, and validation logic

---
+++ src/modules/card/Fee/controller/fee.controller.ts
@@ -0,0 +1,94 @@
+import * as yup from 'yup';
+
+import { Request, Response } from "express";
+import { createFeeSchema, idSchema, updateFeeSchema } from '../../../../utils/validator'
+import { createCardFee, updateCardFee, getCardFees, deleteCardFee } from '../../services/card-fee.service';
+import { abort } from 'process';
+import { DataType } from 'sequelize-typescript';
+
+
+
+
+const feeController = {
+  createFee: async (req: Request, res:Response ) => {
+    try {
+      const createFeeData = await createFeeSchema.validate(req.body, {abortEarly: false});
+      const create = await createCardFee(createFeeData);
+      return res.status(create.statusCode).json(create)
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Validation failed',
+          errors: error.inner.map(err => ({
+            field: err.path,
+            message: err.message
+          }))
+        });
+      }
+
+      console.error('Fee Creation Error:', error);
+
+      return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  },
+
+  updateFee: async (req: Request, res: Response) => {
+    try {
+      const { id } = await idSchema.validate(req.params, {abortEarly: false});
+      const feeData = await updateFeeSchema.validate(req.body, {abortEarly: false});
+
+      const feeUpdate = await updateCardFee(id, feeData);
+      if (feeUpdate)
+      return res.status(feeUpdate.statusCode).json({
+        status: feeUpdate.status,
+        message: feeUpdate.message,
+        data: feeUpdate.data
+      })
+
+    } catch (error) {
+      if (error instanceof yup.ValidationError) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Validation failed',
+          errors: error.inner.map(err => ({
+            field: err.path,
+            message: err.message
+          }))
+        });
+      }
+
+      console.error('Fee update Error:', error);
+
+      return res.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+        error: error instanceof Error ? error.message : 'Unknown error'
+      });
+    }
+  },
+
+  getAllFee: async (req: Request, res: Response) => {
+    try {
+      const getFees = await getCardFees();
+      return 
+    } catch (error) {
+      
+    }
+  },
+
+  deleteFee: async (req: Request, res: Response) => {
+    try {
+      
+    } catch (error) {
+      
+    }
+  }
+
+}
+
+export default feeController;
